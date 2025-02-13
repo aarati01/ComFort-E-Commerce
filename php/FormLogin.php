58b54233079f5c8018269564ebcedee5ba40885c
aarati01 <131001442+aarati01@users.noreply.github.com>
2025-02-12 22:50:33 -0500

    new db setup

--- php/FormLogin.php
+++ php/FormLogin.php
@@ -2,14 +2,24 @@
 require_once "config.php";
 mysqli_set_charset($conn, 'utf8');
 
-// Define database connection variables
-$servername = "db";               // Change from 'localhost' to 'db'
-$username = "web";                // Keep the same if the user exists in the Docker database
-$password = "web";                // Password remains the same
-$dbname = "signup_db";           
+// // Define database connection variables
+// $servername = "db";               // Change from 'localhost' to 'db'
+// $username = "web";                // Keep the same if the user exists in the Docker database
+// $password = "web";                // Password remains the same
+// $dbname = "signup_db";           
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+// // Create connection
+// $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 // Check connection
 if (!$conn) {
--- php/config.php
+++ php/config.php
@@ -1,11 +1,21 @@
 <?php
-   $servername = "db";               // Change from 'localhost' to 'db'
-   $username = "web";                // Keep the same if the user exists in the Docker database
-   $password = "web";                // Password remains the same
-   $dbname = "signup_db";           
+//    $servername = "db";               // Change from 'localhost' to 'db'
+//    $username = "web";                // Keep the same if the user exists in the Docker database
+//    $password = "web";                // Password remains the same
+//    $dbname = "signup_db";           
 
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
+//     $conn = mysqli_connect($servername, $username, $password, $dbname);
 
+
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
